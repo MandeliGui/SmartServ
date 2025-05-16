746c99654066eb1386be8e720cc5c6c2f129cf84
Guilherme Mandeli <guimandeli.santos@gmail.com>
2025-05-16 10:53:41 -0300

    adicionado materiais ao menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,7 @@
         <flux:navlist.item icon="wrench-screwdriver" href="{{ route('tecnico') }}">Tecnicos</flux:navlist.item>
         <flux:navlist.item icon="banknotes" href="{{ route('formas-pagamento') }}">Formas Pagamento</flux:navlist.item>
         <flux:navlist.item icon="tag" href="{{ route('grupo-cliente') }}">Grupo Clientes</flux:navlist.item>
+        <flux:navlist.item icon="wrench-screwdriver" href="{{ route('materiais') }}">Materiais</flux:navlist.item>
         <flux:navlist.item icon="user-group" href="{{ route('usuarios') }}">Usuarios</flux:navlist.item>
     </flux:navlist>
     <flux:spacer/>
