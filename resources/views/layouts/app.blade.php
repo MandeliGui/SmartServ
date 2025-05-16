c796755191751068d4fbebaca05048b571171df6
Guilherme Mandeli <guimandeli.santos@gmail.com>
2025-05-16 13:21:44 -0300

    feito feature de atendentes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
         <flux:navlist.item icon="banknotes" href="{{ route('formas-pagamento') }}">Formas Pagamento</flux:navlist.item>
         <flux:navlist.item icon="tag" href="{{ route('grupo-cliente') }}">Grupo Clientes</flux:navlist.item>
         <flux:navlist.item icon="wrench-screwdriver" href="{{ route('materiais') }}">Materiais</flux:navlist.item>
+        <flux:navlist.item icon="phone-x-mark" href="{{ route('materiais') }}">Atendentes</flux:navlist.item>
         <flux:navlist.item icon="user-group" href="{{ route('usuarios') }}">Usuarios</flux:navlist.item>
     </flux:navlist>
     <flux:spacer/>
