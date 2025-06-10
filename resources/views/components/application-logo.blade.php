4e7a2d79f5783e6dbeb3997425cca656b0d52ffd
Guilherme Mandeli <guimandeli.santos@gmail.com>
2025-06-10 14:30:44 -0300

    ajustado tela de login

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('dashboard') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-smartserv-color-neutral-100">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-neutral-100">
                 <span
                     class="text-accent">S</span>mart<span
                     class="text-accent">Serv</span>
