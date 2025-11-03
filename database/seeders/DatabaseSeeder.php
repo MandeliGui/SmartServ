d9abeb97fcaae1973b51d9da6e0898f53fc7189f
Guilherme Mandeli <guimandeli.santos@gmail.com>
2025-11-03 09:32:05 -0300

    criado cliente hidrolon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,61 +20,82 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::create([
-            'name'          => 'Guilherme Mandeli',
-            'razao_social'  => 'Guilherme Mandeli',
-            'nome_fantasia' => 'Guilherme Mandeli',
-            'email'         => 'guimandeli.santos@gmail.com',
-            'password'      => bcrypt('12345678'),
-            'cpf_cnpj'      => '12706497955',
-            'telefone'      => '43998442622',
-        ]);
+//        User::create([
+//            'name'          => 'Guilherme Mandeli',
+//            'razao_social'  => 'Guilherme Mandeli',
+//            'nome_fantasia' => 'Guilherme Mandeli',
+//            'email'         => 'guimandeli.santos@gmail.com',
+//            'password'      => bcrypt('12345678'),
+//            'cpf_cnpj'      => '12706497955',
+//            'telefone'      => '43998442622',
+//        ]);
+//
+//        User::create([
+//            'name'          => 'Jessica Balan',
+//            'razao_social'  => 'Jessica Balan',
+//            'nome_fantasia' => 'Jessica Balan',
+//            'email'         => 'jess@gmail.com',
+//            'password'      => bcrypt('12345678'),
+//            'cpf_cnpj'      => '11441384936',
+//            'telefone'      => '4396536081',
+//        ]);
 
         User::create([
-            'name'          => 'Jessica Balan',
-            'razao_social'  => 'Jessica Balan',
-            'nome_fantasia' => 'Jessica Balan',
-            'email'         => 'jess@gmail.com',
+            'name'          => 'Gelson Emilio dos Santos',
+            'razao_social'  => 'Hidrolondrina Encanamentos e Serviços Hidráulicos Ltda',
+            'nome_fantasia' => 'Hidrolondrina',
+            'email'         => 'gelson.emilio@gmail.com',
             'password'      => bcrypt('12345678'),
-            'cpf_cnpj'      => '11441384936',
-            'telefone'      => '4396536081',
+            'cpf_cnpj'      => '63378287000108',
+            'telefone'      => '43998210135',
         ]);
 
+
+//        Usuario::create([
+//            'is_admin' => true,
+//            'nome'     => 'Guilherme Mandeli',
+//            'email'    => 'guimandeli.santos@gmail.com',
+//            'password' => bcrypt('12345678'),
+//            'removido' => false,
+//            'user_id'  => 1,
+//        ]);
+//        Usuario::create([
+//
+//            'nome'     => 'Leonardo Mandeli',
+//            'email'    => 'leomandeli.santos@gmail.com',
+//            'password' => bcrypt('12345678'),
+//            'removido' => false,
+//            'user_id'  => 1,
+//        ]);
+//
+//        Usuario::create([
+//            'is_admin' => true,
+//            'nome'     => 'Jessica Balan',
+//            'email'    => 'jess@gmail.com',
+//            'password' => bcrypt('12345678'),
+//            'removido' => false,
+//            'user_id'  => 2,
+//        ]);
 
         Usuario::create([
             'is_admin' => true,
-            'nome'     => 'Guilherme Mandeli',
-            'email'    => 'guimandeli.santos@gmail.com',
+            'nome'     => 'Gelson Emilio dos Santos',
+            'email'    => 'gelson.emilio@gmail.com',
             'password' => bcrypt('12345678'),
             'removido' => false,
-            'user_id'  => 1,
-        ]);
-        Usuario::create([
-
-            'nome'     => 'Leonardo Mandeli',
-            'email'    => 'leomandeli.santos@gmail.com',
-            'password' => bcrypt('12345678'),
-            'removido' => false,
-            'user_id'  => 1,
+            'user_id'  => 3,
         ]);
 
-        Usuario::create([
-            'is_admin' => true,
-            'nome'     => 'Jessica Balan',
-            'email'    => 'jess@gmail.com',
-            'password' => bcrypt('12345678'),
-            'removido' => false,
-            'user_id'  => 2,
-        ]);
 
-        CategoriaEntradaSaidaModel::create([
-            'id'        => -1,
-            'nome'      => 'Ordens de Serviço',
-            'tipo'      => TipoEntradaSaida::ENTRADA->value,
-            'descricao' => null,
-            'removido'  => 0,
-            'user_id'   => null,
-        ]);
+//
+//        CategoriaEntradaSaidaModel::create([
+//            'id'        => -1,
+//            'nome'      => 'Ordens de Serviço',
+//            'tipo'      => TipoEntradaSaida::ENTRADA->value,
+//            'descricao' => null,
+//            'removido'  => 0,
+//            'user_id'   => null,
+//        ]);
 
     }
 }
