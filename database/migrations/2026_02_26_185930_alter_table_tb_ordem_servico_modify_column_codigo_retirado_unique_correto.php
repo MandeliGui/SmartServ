7da4d84ec1e2abe877303d15080fc2a4f28f3928
Guilherme Mandeli <guimandeli.santos@gmail.com>
2026-02-26 16:02:16 -0300

    retirado unique codigo ordem servico att

---
+++ database/migrations/2026_02_26_185930_alter_table_tb_ordem_servico_modify_column_codigo_retirado_unique_correto.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tb_ordem_servico', function (Blueprint $table) {
+            $table->dropUnique(['codigo']); // ou nome do índice
+            $table->string('codigo')->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tb_ordem_servico', function (Blueprint $table) {
+            $table->string('codigo')->unique()->change();
+
+        });
+    }
+};
